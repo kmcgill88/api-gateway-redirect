a07572cc33fd08f9e150178371becd18a247991f
Kevin McGill <kevin@mcgilldevtech.com>
2018-01-15 22:15:26 -0600

    Add record sets and acm.
    Update run.sh

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A serverless solution to handle url redirects.
 
 
 ## Setup
-- Set your own default values for the CloudFormations template.
-- Run `bash run.sh deploy`
-- Profit!
+- Run `bash run.sh deploy <FromDomain> <ToDomain> <ToProtocol> <HostedZoneId> <AcmArnForFromDomain>`
+  - For example `bash run.sh deploy www.mcgilldevtech.com mcgilldevtech.com https ABC123 arn:aws:acm:us-east-1:123456789012:certificate/abc123` to redirect www to your apex domain.
+- Be patient, deploying the `AWS::ApiGateway::DomainName` doesn't break and speed records!
+- Hit your `FromDomain` and you should be redirected to your `ToDomain`
+> Swap out `deploy` with `update` to run again.
--- run.sh
+++ run.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 if [[ $1 == "deploy" ]]; then
-  aws cloudformation create-stack --stack-name URL-REDIRECTER --capabilities CAPABILITY_NAMED_IAM --template-body file://./redirect.template.yaml
+  aws cloudformation create-stack \
+    --stack-name URL-REDIRECTER \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --template-body file://./redirect.template.yaml \
+    --parameters \
+        ParameterKey=FromDomain,ParameterValue=$2 \
+        ParameterKey=ToProtocol,ParameterValue=$3 \
+        ParameterKey=ToDomain,ParameterValue=$4 \
+        ParameterKey=HostedZoneId,ParameterValue=$5 \
+        ParameterKey=FromDomainAcmArn,ParameterValue=$6
 fi
 if [[ $1 == "update" ]]; then
-  aws cloudformation update-stack --stack-name URL-REDIRECTER --capabilities CAPABILITY_NAMED_IAM --template-body file://./redirect.template.yaml
+  aws cloudformation update-stack \
+    --stack-name URL-REDIRECTER \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --template-body file://./redirect.template.yaml
+    --parameters \
+        ParameterKey=FromDomain,ParameterValue=$2 \
+        ParameterKey=ToProtocol,ParameterValue=$3 \
+        ParameterKey=ToDomain,ParameterValue=$4 \
+        ParameterKey=HostedZoneId,ParameterValue=$5 \
+        ParameterKey=FromDomainAcmArn,ParameterValue=$6
 fi
 if [[ $1 == "delete" ]]; then
   aws cloudformation delete-stack --stack-name URL-REDIRECTER
